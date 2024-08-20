76d401719d815b7995da31ef37665841e4474f8f
wendel filipe <wendelfilipe1997@hotmail.com>
2024-08-20 11:57:55 -0300

    c#

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Loop for");
+
+for(int i = 0, j = 0; i+j <= 20; i++, j++)
+{
+    Console.WriteLine($"i = {i} e j = {j}");
+}
+
+Console.WriteLine("\nFim do processamento...");
+    Console.ReadKey();
+
