dcadb81ec500f9d51c6bde383be3ebbf34a01f78
wendel filipe <wendelfilipe1997@hotmail.com>
2024-08-23 11:00:56 -0300

    exercice static field

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Loop for");
+using System.Runtime.Intrinsics.Arm;
 
-for(int i = 0, j = 0; i+j <= 20; i++, j++)
+Console.WriteLine("Campos estáticos");
+
+ContaCorrente c1 = new();
+
+c1.Conta = 101;
+c1.Nome = "Maria";
+// c1.Juros = 3.99f;
+
+ContaCorrente c2 = new();
+
+c2.Conta = 102;
+c2.Nome = "Marta";
+// c2.Juros = 4.99f;
+
+ContaCorrente.Juros = 4.25f;
+
+Console.WriteLine($"Cliente : {c1.Nome} - Juros Anual : {c1.JurosAnual()}");
+Console.WriteLine($"Cliente : {c2.Nome} - Juros Anual : {c2.JurosAnual()}");
+
+Console.ReadKey();
+public class ContaCorrente
 {
-    Console.WriteLine($"i = {i} e j = {j}");
+    public int Conta;
+    public string? Nome;
+    public static float Juros;
+
+    public float JurosAnual()
+    {
+        return Juros * 12;
+    }
 }
 
-Console.WriteLine("\nFim do processamento...");
-    Console.ReadKey();
+
 
