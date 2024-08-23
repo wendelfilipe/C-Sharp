fd9d3faaec6ed4efe2fda309fabbdf2614da310d
wendel filipe <wendelfilipe1997@hotmail.com>
2024-08-23 12:21:08 -0300

    struct

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Loop for");
+Console.WriteLine("Struct");
 
-for(int i = 0, j = 0; i+j <= 20; i++, j++)
+Cliente cliente = new();
+
+cliente.Name = "Maria";
+cliente.Idade = 19;
+
+Console.WriteLine($"{cliente.Name} {cliente.Idade}");
+
+Console.ReadKey();
+
+public struct Cliente
 {
-    Console.WriteLine($"i = {i} e j = {j}");
+    public string Name { get; set; }
+    public int Idade { get; set; }
+
+    public Cliente(string name, int idade)
+    {
+        Name = name;
+        Idade = idade;
+    }
 }
-
-Console.WriteLine("\nFim do processamento...");
-    Console.ReadKey();
-
