1e728d088ec2bc91b354169466f86bace40f58ad
wendel filipe <wendelfilipe1997@hotmail.com>
2024-08-30 10:07:40 -0300

    ArrayList 4

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Collections;
 
-Console.WriteLine("ArrayList 2");
+Console.WriteLine("ArrayList 4");
 
 var lista = new ArrayList()
 {
     "Maria", 5 , true, false, 6.0, 5, 0, 89, 0, 45, 0, 0
 };
 
+var lista2 = new ArrayList()
+{
+    "Maria", "Ana", "Bruna", "Carol", "Thais"
+};
+
+lista2.Sort();
+
+var res = lista2.Contains("Maria");
+
+lista2.Clear();
 
 lista.Remove(0);
 
@@ -16,7 +26,9 @@ lista.RemoveAt(4);
 lista.RemoveRange(0, 2);
 
 
-foreach(var list in lista)
+foreach(var list in lista2)
 {
     Console.WriteLine(list);
 }
+
+Console.WriteLine(res);
