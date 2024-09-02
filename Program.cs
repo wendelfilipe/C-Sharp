6f79d609a178f5185fa283cc904371424a421707
wendel filipe <wendelfilipe1997@hotmail.com>
2024-09-02 11:09:13 -0300

    Mega sena

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Loop for");
+Console.WriteLine("Classe random ");
 
-for(int i = 0, j = 0; i+j <= 20; i++, j++)
+Random random = new Random();
+
+// Console.WriteLine(random.Next());
+// Console.WriteLine(random.Next(20));
+// Console.WriteLine(random.Next(10, 20));
+// Console.WriteLine(random.NextDouble());
+
+
+int[] numerosSorteados = new int[7];
+
+for (int i = 0; i < 7; i++)
 {
-    Console.WriteLine($"i = {i} e j = {j}");
+    int numerosAleatorios;
+
+    do
+    {
+        numerosAleatorios = random.Next(1, 61);
+    }
+    while (numerosSorteados.Contains(numerosAleatorios));
+
+    numerosSorteados[i] = numerosAleatorios; 
 }
 
-Console.WriteLine("\nFim do processamento...");
-    Console.ReadKey();
+Array.Sort(numerosSorteados);
 
+Console.WriteLine("Números Sorteados\n");
+
+Console.WriteLine(string.Join(" ", numerosSorteados));
