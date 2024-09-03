3a426bf8d2a41bd3488337fc9f117bf9a4c36f79
wendel filipe <wendelfilipe1997@hotmail.com>
2024-09-03 11:39:00 -0300

    as e is casting

--- Program.cs
+++ Program.cs
@@ -1,25 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using C_;
 
-Console.WriteLine("Upcasting e Downcasting");
+Console.WriteLine("as e");
 
 // upcasting
 Circulo circulo = new Circulo(20, 20);
 Forma forma = circulo;
 
-forma.DesenharForma();
+// Downcasting e as
 
-// Downcasting
+Circulo c = forma as Circulo;
 
-Circulo c = (Circulo)forma;
+// usando is
 
-c.DesenharForma();
-c.PintarCirculo();
+if (forma is Circulo)
+{
+    Console.WriteLine("Converteu forma para circulo");
+    var tipo = forma as Circulo;
+    tipo?.PintarCirculo();
+}
+else
+{
+    Console.WriteLine("Operação de downcasting não é possivel");
+}
 
-Console.WriteLine(c == forma);
-Console.WriteLine(c == circulo);
+if( c != null)
+{       
+    c.DesenharForma();
+    c.PintarCirculo();
+    Console.WriteLine(c == forma);
+    Console.WriteLine(c == circulo);    
+}
+else
+{
+    Console.WriteLine("Erro ao fazer o casting");
+}
 
 
+Object carroObj = new Carro();
+
+try
+{
+    var y = (string)carroObj;
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+// usando as
+
+var x = carroObj as string;
+
+if(x != null)
+{
+    Console.WriteLine("Casting feito com sucesso");
+}
+else
+{
+    Console.WriteLine("Não foi possivel converter");
+}
+
+
+
+class Carro
+{}
 
 
 
