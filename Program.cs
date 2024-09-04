2973cacc2dd31009402d1f35768ba96ef38674a9
wendel filipe <wendelfilipe1997@hotmail.com>
2024-09-04 12:08:00 -0300

    Generics 2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Loop for");
+Console.WriteLine("Generics");
 
-for(int i = 0, j = 0; i+j <= 20; i++, j++)
+var teste2 = new Teste2<int, int>();
+
+int p1 = 10;
+int p2 = 10;
+
+
+teste2.Comparar(p1, p2);
+class Teste
 {
-    Console.WriteLine($"i = {i} e j = {j}");
+    public void Comparar<T>(T p1, T p2)
+    {
+        Console.WriteLine($"Os tipos : {p1.GetType()} e {p2.GetType()}" );
+        var result = p1.Equals(p2);
+        Console.WriteLine($" {p1} e {p2} são iguais ? {result}\n");
+    }
 }
 
-Console.WriteLine("\nFim do processamento...");
-    Console.ReadKey();
-
+class Teste2<T1, T2> where T1 : struct where T2 : struct
+{
+    public void Comparar(T1 p1, T2 p2)
+    {
+        Console.WriteLine($"Os tipos : {p1.GetType()} e {p2.GetType()}" );
+        var result = p1.Equals(p2);
+        Console.WriteLine($" {p1} e {p2} são iguais ? {result}\n");
+    }
+}
