1410700eca27e6cff6bae9d70abc2041c19d11f2
wendel filipe <wendelfilipe1997@hotmail.com>
2024-09-10 09:17:18 -0300

    stack

--- Program.cs
+++ Program.cs
@@ -1,11 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Loop for");
+Console.WriteLine("Stack");
 
-for(int i = 0, j = 0; i+j <= 20; i++, j++)
+Stack<int> numeros = new Stack<int>();
+
+numeros.Push(10);
+numeros.Push(20);
+numeros.Push(30);
+numeros.Push(10);
+
+Console.WriteLine($"A pilha original contém {numeros.Count} itens");
+
+ExibirPilha(numeros);
+
+// retorna sem remover
+Console.WriteLine($"\n Item obtido da pilha (Peek) : { numeros.Peek()}");
+
+// remove e retorna
+Console.WriteLine($"\n Item obtido da pilha (Pop) : { numeros.Pop()}");
+
+Console.WriteLine($"\nA pilha original contém {numeros.Count} itens");
+
+ExibirPilha(numeros);
+
+if(numeros.Contains(20))
+   Console.WriteLine("\n Item 20 esta na pilha");
+else
+    Console.WriteLine("\n Item 20 nao esta na pilha");
+
+Console.WriteLine("\n Copiar a pilha usando ToArray");
+var copia = new Stack<int>(numeros.ToArray());
+ExibirPilha(copia);
+
+Console.WriteLine("\n Removendo todos os itens da pilha\n");
+numeros.Clear();
+Console.WriteLine($"{numeros.Count} itens na pilha");
+
+ExibirPilha(numeros);
+
+static void ExibirPilha<T>(IEnumerable<T> numeros)
 {
-    Console.WriteLine($"i = {i} e j = {j}");
+    
+    Console.WriteLine();
+    foreach (var item in numeros)
+    {
+        Console.WriteLine(item);
+    }
 }
-
-Console.WriteLine("\nFim do processamento...");
-    Console.ReadKey();
-
