4c1708c76f2eb1935c3e4e33a6e5cbe26536f5fd
wendel filipe <wendelfilipe1997@hotmail.com>
2024-09-10 09:40:38 -0300

    Stack II

--- Program.cs
+++ Program.cs
@@ -1,11 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Loop for");
+Console.WriteLine("Stack II");
 
-for(int i = 0, j = 0; i+j <= 20; i++, j++)
+EditorTexto editor = new EditorTexto();
+
+Console.WriteLine("\n Digitando...");
+
+editor.DigitarChar('S');
+editor.DigitarChar('t');
+editor.DigitarChar('a');
+editor.DigitarChar('c');
+editor.DigitarChar('q');
+
+Console.WriteLine("\nFazendo o Undo... \n");
+editor.Undo();
+editor.Undo();
+
+Console.WriteLine("\nRedigitando ... \n");
+editor.DigitarChar('c');
+editor.DigitarChar('k');
+
+
+public class EditorTexto
 {
-    Console.WriteLine($"i = {i} e j = {j}");
-}
+    private Stack<char> undoStack = new Stack<char>();
+    private string texto = "";
 
-Console.WriteLine("\nFim do processamento...");
-    Console.ReadKey();
+    public void DigitarChar(char c)
+    {
+        texto += c;
+        undoStack.Push(c);
+        Console.WriteLine($"Texto : {texto}");
+    }
+
+    public void Undo()
+    {
+        if(undoStack.Count() > 0)
+        {
+            char ultimoChar = undoStack.Pop();
+            texto = texto.Substring(0, texto.Length - 1);
+            Console.WriteLine($"Texto : {texto}");
+        }
+    }
+}
 
