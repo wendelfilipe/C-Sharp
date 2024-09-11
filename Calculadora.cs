ee68cdb1e0e9d120ea582e7095cd655c128882ca
wendel filipe <wendelfilipe1997@hotmail.com>
2024-09-11 10:04:02 -0300

    Delegate

---
+++ Calculadora.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    public class Calculadora
+    {
+        public static float Somar(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+        public static float Multiplicar(float n1, float n2)
+        {
+            return n1 * n2;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Loop for");
+using C_;
 
-for(int i = 0, j = 0; i+j <= 20; i++, j++)
-{
-    Console.WriteLine($"i = {i} e j = {j}");
-}
+Console.WriteLine("Delegates");
 
-Console.WriteLine("\nFim do processamento...");
-    Console.ReadKey();
+// DelegateCalc calc = new DelegateCalc(Calculadora.Somar);
+// var result = calc.Invoke(20, 30);
+// Console.WriteLine(result);
 
+
+DelegateCalc calc2 = Calculadora.Somar;
+var result2 = calc2(20, 30);
+Console.WriteLine(result2);
+
+DelegateCalc calc3 = Calculadora.Multiplicar;
+var result3 = calc3(20, 30);
+Console.WriteLine(result3);
+
+
+// DelegateCalc calc3 = (float x, float y) => Calculadora.Somar(x, y);
+// var result3 = calc3(20, 30);
+// Console.WriteLine(result3);
+
+public delegate float DelegateCalc(float x , float y);
