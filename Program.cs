96cca017754d17ab612c73e55d628f9a90817850
wendel filipe <wendelfilipe1997@hotmail.com>
2024-09-12 09:50:41 -0300

    Delegate Action

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Loop for");
+Console.WriteLine("Delegate Action");
 
-for(int i = 0, j = 0; i+j <= 20; i++, j++)
-{
-    Console.WriteLine($"i = {i} e j = {j}");
-}
+var numero = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("\nFim do processamento...");
-    Console.ReadKey();
 
+Action<int> dobra = x => Console.WriteLine(x * 2);
+
+dobra(numero);
